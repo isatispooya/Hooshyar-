2eeaf5f4db301fcb096197c8d6c173a5b06a3aed
Mahya Abouei <mahyaaboueii@gmail.com>
2024-07-17 13:15:45 +0330

    pay

--- Pay/views.py
+++ Pay/views.py
@@ -10,6 +10,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from Authentication import fun
 from Visit.models import KindOfCounseling
+from Authentication.models import Auth
 from Visit.serializers import KindOfCounselingSerializer
 import json
 
@@ -21,19 +22,39 @@ class PayViewset(APIView):
                 return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
             
             user = fun.decryptionUser(Authorization)
+            user = user.first()
             if not user:
                 return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-            
             code = request.query_params.get('code')
-            
+            national_code = user.national_code
+            print('farasahm_user')
+
+            rest_api_token = 'ZtqX2dtvjxyYwnjInl8xGhGiynj5uKiO'
+            data = {'token' : rest_api_token , 'nc': national_code}
+            data = json.dumps(data)
+            headers = {'Content-Type': 'application/json'}
+            farasahm_user_asset = requests.post('https://b.fidip.ir/service/assetcustomer',data = data , headers=headers)
+            if farasahm_user_asset.status_code == 200:
+                try:
+                    farasahm_user_asset = farasahm_user_asset.json()
+                    off_asset = {'broker':farasahm_user_asset['broker']*0.1}
+                    print(off_asset)
+
+                    for i in farasahm_user_asset['asset']:
+                        off_asset[i['Symbol']] = int(int(i['VolumeInPrice'])/10000000)/100
+                        print(off_asset)
+                except ValueError:
+                    off_asset = {'broker': 0, 'بتیس1': 0, 'خاتم1': 0, 'ترمه1': 0, 'ویسا1': 0}
+                    return Response({'error': 'Failed to parse JSON response from external service'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
             kindofcounseling = KindOfCounseling.objects.filter(id=kind).first()
             serializer_kindofcounseling = KindOfCounselingSerializer(kindofcounseling)
             result = {'price' :int (serializer_kindofcounseling.data['price'])}
-
-            off = 0
+            
             try :
-                 
                 discount = models.Discount.objects.filter(code=code).first()
+                print(code)
                 serializer_discount= serializers.DiscountSerializer(discount)
 
                 expire_discount = serializer_discount.data ['expiration_date']
@@ -60,8 +81,16 @@ class PayViewset(APIView):
             except :
                 pass
 
-            result ['off']= off
-            result['final_price'] = result['price'] - off
+            result ['off'] = off
+            result ['off_broker'] = result['price'] * off_asset['broker']
+            result ['off_betis'] = result['price'] * off_asset['بتیس1']
+            result ['off_khatam'] = result['price'] * off_asset['خاتم1']
+            result ['off_terme'] = result['price'] * off_asset['ترمه1']
+            result ['off_visa'] = result['price'] * off_asset['ویسا1']
+            result ['off_all'] = result ['off'] + result ['off_betis'] + result ['off_khatam'] + result ['off_terme'] + result ['off_visa']
+            result ['off_all'] = min(result ['off_all'], result['price'])
+
+            result['final_price'] = result['price'] - result ['off_all']
             result['tax'] = int (result ['final_price'] * 0.1)
             result['pey'] = int(result['final_price'] + result ['tax'])
             return Response(result, status=status.HTTP_200_OK)           
@@ -94,9 +123,6 @@ class DiscountViewset (APIView) :
         number_of_times = serializer_discount.data['number_of_times']
 
 
-
-
-
         # فعلا تعداد دفعات بیشتر از 0  میزاریم بعدش درست میکنیم
                 
         if number_of_times > 0 and expire_discount >  now :
@@ -105,21 +131,3 @@ class DiscountViewset (APIView) :
 
 
 
-
-
-class invoice (APIView) :
-    def post  (self ,request) :
-        Authorization = request.headers.get('Authorization')
-        if not Authorization:
-            return Response({'error': 'Authorization header is missing'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        user = fun.decryptionUser(Authorization)
-        if not user:
-            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
-        national_code = request.data.get('national_code')
-        rest_api_token = 'ZtqX2dtvjxyYwnjInl8xGhGiynj5uKiO'
-        data = {'token' : rest_api_token , 'nc': national_code}
-        data = json.dumps(data)
-        headers = {'Content-Type': 'application/json'}
-        farasahm_user = requests.post('http://b.fidip.ir/service/assetcustomer',data = data , headers=headers)
-        return Response({'message' : 'ok'})
